376948c4b798f6212ada79682b94ecfb04a7899b
radu <rpopescu@gmail.com>
2009-01-19 09:11:48 +0100

    first working version

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+// GPS/Photo timestamp matching resolution, in seconds.
+define(MATCH_TOLERANCE, 5); 
+
+// TRUE for verbose, debugging messages, FALSE to suppress these.
+define(TAGGER_DEBUG, FALSE); 
+
+// TRUE to skip changing the photos (dry run), FALSE to change the photos with GPS information.
+define(DRY_RUN, FALSE);
+
+?>
